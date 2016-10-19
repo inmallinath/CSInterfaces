587ea1ee0aaa8ece585ca48a4ebb6cdd4ff39f73
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-18 20:34:07 -0700

    Create a concrete class that represents a polygon with a constructor that takes two parameters of side and length

--- CSInterfaces.Library/ConcretePolygon.cs
+++ CSInterfaces.Library/ConcretePolygon.cs
@@ -8,5 +8,25 @@ namespace CSInterfaces.Library
 {
     public class ConcretePolygon
     {
+        public int NumberOfSides { get; set; }
+        public int Length { get; set; }
+
+        public ConcretePolygon(int sides, int length)
+        {
+            NumberOfSides = sides;
+            Length = length;
+        }
+
+        public double GetPerimeter()
+        {
+            double perimeter;
+            perimeter = Length * NumberOfSides;
+            return perimeter;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
