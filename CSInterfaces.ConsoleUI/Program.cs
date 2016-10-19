72d7f425cb25757224d45504df851cbdb83597da
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-18 21:13:17 -0700

    Add a derived class to inherit the base class. Add a console program to display the results of using the concrete class

---
+++ CSInterfaces.ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using CSInterfaces.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSInterfaces.ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square(4);
+            ShowShape("Square", square);
+        }
+
+        public static void ShowShape(string shapeType, ConcretePolygon shapeName)
+        {
+            try
+            {
+                Console.WriteLine("{0} Number of Sides {1}", shapeType, shapeName.NumberOfSides);
+                Console.WriteLine("{0} Length: {1}", shapeType, shapeName.Length);
+                Console.WriteLine("{0} Perimeter: {1}", shapeType, shapeName.GetPerimeter());
+                Console.WriteLine("{0} Area: {1}", shapeType, shapeName.GetArea());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception for {0}:\n {1} " + shapeType, ex.GetType().Name);
+            }
+        }
+    }
+}
+ 
---
+++ CSInterfaces.Library/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSInterfaces.Library
+{
+    public class Square : ConcretePolygon
+    {
+        public Square(int length) : base(4, length)
+        {
+        }
+
+        public override double GetArea()
+        {
+            return Length * Length;
+        }
+    }
+}
