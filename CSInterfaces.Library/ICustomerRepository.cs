f5c2971189b5722369802980ed04b6748ad94580
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-21 04:44:33 -0700

    Remove Errors in the XAML; Add a repository interface to perform CRUD operations

---
+++ CSInterfaces.Library/ICustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSInterfaces.Library
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetCustomer();
+
+        Customer GetCustomer(string lastName);
+
+        void AddCustomer(Customer customer);
+
+        void UpdateCustomer(string lastName, Customer updatedCustomer);
+
+        void DeleteCustomer(string lastName);
+
+        void UpdateCustomers(IEnumerable<Customer> updatedCustomers);
+
+    }
+}
