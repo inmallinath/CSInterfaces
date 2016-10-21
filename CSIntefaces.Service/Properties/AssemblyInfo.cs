d7332a81a17fedde94b78ed195e913a6b1f56c36
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-21 05:16:19 -0700

    Add a new Service Application to mock pulling data from a service

--- CSIntefaces.Service/Properties/AssemblyInfo.cs
+++ CSIntefaces.Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSInterfaces.Service")]
+[assembly: AssemblyTitle("CSIntefaces.Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSInterfaces.Service")]
+[assembly: AssemblyProduct("CSIntefaces.Service")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b35f4356-9a06-471d-bac5-e5f7ea5cfa4b")]
+[assembly: Guid("32382d77-cea1-4160-ba27-3bfef4e57fc9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
