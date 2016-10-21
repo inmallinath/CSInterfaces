c984240115218213144d684fa608941dfce73ddb
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-21 05:43:35 -0700

    Make changes to the mock data to reflect the source (service)

--- CSIntefaces.Service/CustomerService.svc.cs
+++ CSIntefaces.Service/CustomerService.svc.cs
@@ -5,17 +5,17 @@ using CSInterfaces.Library;
 namespace CSIntefaces.Service
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    // NOTE: In order to launch WCF Service Client for Serviceing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class CustomerService : ICustomerService
     {
         public List<Customer> GetCustomers()
         {
             var customers = new List<Customer>()
             {
-                new Customer() { FirstName = "One", LastName = "Test1", EmailAddress = "One@Test1.com", Rating = 5, StartDate = new DateTime(1977, 11, 23) },
-                new Customer() { FirstName = "Two", LastName = "Test2", EmailAddress = "Two@Test2.com", Rating = 7, StartDate = new DateTime(1978, 11, 23) },
-                new Customer() { FirstName = "Three", LastName = "Test3", EmailAddress = "Three@Test3.com", Rating = 8, StartDate = new DateTime(1979, 11, 23) },
-                new Customer() { FirstName = "Four", LastName = "Test4", EmailAddress = "Four@Test4.com", Rating = 9, StartDate = new DateTime(1976, 11, 23) }
+                new Customer() { FirstName = "One", LastName = "Service1", EmailAddress = "One@Service1.com", Rating = 5, StartDate = new DateTime(1977, 11, 23) },
+                new Customer() { FirstName = "Two", LastName = "Service2", EmailAddress = "Two@Service2.com", Rating = 7, StartDate = new DateTime(1978, 11, 23) },
+                new Customer() { FirstName = "Three", LastName = "Service3", EmailAddress = "Three@Service3.com", Rating = 8, StartDate = new DateTime(1979, 11, 23) },
+                new Customer() { FirstName = "Four", LastName = "Service4", EmailAddress = "Four@Service4.com", Rating = 9, StartDate = new DateTime(1976, 11, 23) }
             };
             return customers;
         }
