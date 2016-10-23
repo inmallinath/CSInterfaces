ff699bbc0554cd20983166d83aa218b633c44ed8
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-23 08:45:07 -0700

    Add a sql repository to demo the use of interfaces in pulling data from a relational database repository

---
+++ CSInterfacesRepository.SQL/SQLRepository.cs
@@ -0,0 +1,43 @@
+﻿using CSInterfaces.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSInterfacesRepository.SQL
+{
+    public class SQLRepository : ICustomerRepository
+    {
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+           //EF code goes here to retrieve customers from SQL
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomer(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomer(string lastName, Customer updatedCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomers(IEnumerable<Customer> updatedCustomers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
